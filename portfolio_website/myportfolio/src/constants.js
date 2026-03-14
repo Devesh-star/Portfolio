0dda484ebf058d68ac345ddf8b6424bc402b82e7
Devesh-star <kanjarrbacche@gmail.com>
2026-03-14 23:59:59 +0530

    Add files via upload

---
+++ portfolio_website/myportfolio/src/constants.js
@@ -0,0 +1,209 @@
+
+// Skills Section Logo's
+import htmlLogo from './assets/html.png';
+import cssLogo from './assets/css.png';
+import javascriptLogo from './assets/javascript.png';
+import reactjsLogo from './assets/reactjs.png';
+import tailwindcssLogo from './assets/tailwindcss.png';
+import bootstrapLogo from './assets/bootstrap.png';
+import springbootLogo from './assets/springboot.png';
+import nodejsLogo from './assets/nodejs.png';
+import expressjsLogo from './assets/express.png';
+import mongodbLogo from './assets/mongodb.png';
+import cLogo from './assets/c.png';
+import cppLogo from './assets/cpp.png';
+import javaLogo from './assets/java.png';
+import pythonLogo from './assets/python.png';
+import gitLogo from './assets/git.png';
+import githubLogo from './assets/github.png';
+import vscodeLogo from './assets/vscode.png';
+import postmanLogo from './assets/postman.png';
+import mcLogo from './assets/mc.png';
+import netlifyLogo from './assets/netlify.png';
+import vercelLogo from './assets/vercel.png';
+import webverseLogo from './assets/webverse_logo.png';
+import agcLogo from './assets/agc_logo.png';
+import newtonschoolLogo from './assets/newtonschool_logo.png';
+
+import glaLogo from './assets/gla_logo.png';
+import bsaLogo from './assets/bsa_logo.png';
+import vpsLogo from './assets/vps_logo.png';
+import brainLogo from './assets/brainLogo.jpeg';
+import srmlogo from './assets/srmlogo.webp';
+import resumeBuilder from'./assets/resumeBuilder.png';
+import expenseTracker from'./assets/expenseTracker.png';
+import simonGame from'./assets/simonGame.png';
+
+import githubdetLogo from './assets/github_det.png';
+import csprepLogo from './assets/cs_prep.png';
+import movierecLogo from './assets/movie_rec.png';
+import taskremLogo from './assets/task_rem.png';
+import npmLogo from './assets/npm.png';
+import webverLogo from './assets/web_dig.png';
+import cmLogo from './assets/cm.png';
+import imagesearchLogo from './assets/image_search.png';
+import removebgLogo from './assets/remove_bg.png';
+
+
+export const SkillsInfo = [
+  {
+    title: 'Frontend',
+    skills: [
+      { name: 'HTML', logo: htmlLogo },
+      { name: 'CSS', logo: cssLogo },
+      { name: 'JavaScript', logo: javascriptLogo },
+      { name: 'React JS', logo: reactjsLogo },
+      { name: 'Tailwind CSS', logo: tailwindcssLogo },
+      { name: 'Bootstrap', logo: bootstrapLogo },
+    ],
+  },
+  {
+    title: 'Backend',
+    skills: [
+      { name: 'Node JS', logo: nodejsLogo },
+      { name: 'Express JS', logo: expressjsLogo },
+      { name: 'MongoDB', logo: mongodbLogo },
+    ],
+  },
+  {
+    title: 'Languages',
+    skills: [
+      { name: 'C', logo: cLogo },
+      { name: 'C++', logo: cppLogo },
+      { name: 'Java', logo: javaLogo },
+      { name: 'Python', logo: pythonLogo },
+      { name: 'JavaScript', logo: javascriptLogo },
+    ],
+  },
+  {
+    title: 'Tools',
+    skills: [
+      { name: 'Git', logo: gitLogo },
+      { name: 'GitHub', logo: githubLogo },
+      { name: 'VS Code', logo: vscodeLogo },
+      { name: 'Postman', logo: postmanLogo },
+      { name: 'Compass', logo: mcLogo },
+      { name: 'Vercel', logo: vercelLogo },
+      { name: 'Netlify', logo: netlifyLogo },
+    ],
+  },
+];
+
+  export const experiences = [
+    {
+      id: 0,
+      img: webverseLogo,
+      role: "Fullstack Developer",
+      company: "Webverse Digital",
+      date: "April 2024 - Present",
+      desc: "Developed dynamic and scalable web applications using the MERN stack, handling both frontend and backend development. Collaborated with cross-functional teams to build responsive UI, implement RESTful APIs, and optimize application performance in an agile environment.",
+      skills: [
+        "HTML",
+        "CSS",
+        "JavaScript",
+        "React JS",
+        "TypeScript",
+        "Node JS",
+        "Tailwind CSS",
+        "MongoDb",
+        "Redux",
+        " Next Js",
+      ],
+    },
+    {
+      id: 1,
+      img: agcLogo,
+      role: "Fullstack Engineer",
+      company: "Agumentik Group of Companies",
+      date: "July 2023 - March 2024",
+      desc: "Contributed to innovative projects as a Fullstack Engineer, leading both frontend and backend development using technologies such as HTML, CSS, JavaScript, PHP, SQL, Bootstrap, and ReactJS. Worked closely with the team to deliver responsive, high-performance web applications and improve user experience through seamless integration of various technologies.",
+      skills: [
+        "ReactJS",
+        "Redux",
+        "JavaScript",
+        "Tailwind CSS",
+        "HTML",
+        "CSS",
+        "SQL",
+      ],
+    },
+    {
+      id: 2,
+      img: newtonschoolLogo,
+      role: "Frontend Intern",
+      company: "Newton School",
+      date: "September 2021 - August 2022",
+      desc: "Worked as a Frontend Developer Intern, designing and implementing scalable UI components and responsive websites using HTML, CSS, JavaScript, Bootstrap, and Material UI. Collaborated with the design team to translate wireframes and prototypes from Figma into interactive, user-friendly web pages.",
+      skills: [
+        "HTML",
+        "CSS",
+        "Javascript",
+        "Bootstrap",
+        "Figma",
+        "Material UI",
+      ],
+    },
+  ];
+  
+  export const education = [
+    {
+      id: 1,
+      img: srmlogo,
+      school: "SRM Institute Of Science & TEchnology",
+      date: "2024 - 2028",
+      grade: "7.6cgpa",
+      desc: "Bachelor of Technology (B.Tech) — Currently Pursuing in SRM Institute of Science and Technology. Pursuing an undergraduate degree in engineering with a focus on developing strong analytical, technical, and problem-solving abilities. Gaining hands-on experience through academic coursework, laboratory work, and collaborative projects that emphasize practical application of engineering principles. Actively strengthening skills in critical thinking, technical communication, and modern engineering tools while building a solid foundation for solving real-world technological challenges.",
+      degree: "Bachelor of Science - BSC (Computer Science)",
+    },
+    {
+      id: 2,
+      img: brainLogo,
+      school: "Brain International School, New Delho",
+      date: "Apr 2023 - March 2024",
+      grade: "78%",
+      desc: "I completed my class 12 education from Brain International Schhol, New Delhi, under the CBSE board, where I studied Physics, Chemistry, and Mathematics (PCM) with Computer Science.",
+      degree: "CBSE(XII) - PCM with Computer Science",
+    },
+    {
+      id: 3,
+      img: brainLogo,
+      school: "Brain Internation School, New Delhi",
+      date: "Apr 2021 - March 2022",
+      grade: "87.5%",
+      desc: "I completed my class 10 education from Brain International School, New Delhi, under the CBSE board, where I studied Science with Computer.",
+      degree: "CBSE(X), Science with Computer Application",
+    },
+  ];
+  
+  export const projects = [
+    {
+      id: 0,
+      title: "Expense Tracker",
+      description:
+        "Built a client-side expense management web application using HTML, CSS, and vanilla JavaScript. Implemented DOM manipulation and event handling to add and remove transactions while dynamically calculating total balance, income, and expenses. Focused on clean UI design and efficient data handling for smooth user interaction.",
+      image: expenseTracker,
+      tags: ["HTML", "CSS", "JavaScript"],
+      github: "https://github.com/Devesh-star/Expense-Manager",
+      webapp: "https://githubprofiledetective.netlify.app/",
+    },
+    {
+      id: 1,
+      title: "Resume Builder",
+      description:
+        "Engineered a resume generator web application using React and Tailwind CSS. Implemented modular components, controlled forms, and dynamic state management to capture user input and render real-time resume previews. Designed a responsive UI with Tailwind utilities to ensure accessibility and consistent layout across devices.",
+      image: resumeBuilder,
+      tags: ["React JS", "Node.js", "MongoDB", "Express", "HTML", "CSS", "JavaScript"],
+      github: "https://github.com/Devesh-star/Resume-Builder",
+      webapp: "https://csprep.netlify.app/",
+    },
+    {
+      id: 2,
+      title: "Simon Game",
+      description:
+        "Implemented a digital version of the Simon memory game using HTML, CSS, and JavaScript. Built the core game logic including random sequence generation, user input validation, and progressive difficulty levels. Leveraged event listeners and DOM manipulation to provide real-time feedback through animations and sound effects.",
+      image: simonGame,
+      tags: ["HTML", "CSS", "JavaScript"],
+      github: "https://github.com/Devesh-star/Simon-Game",
+      webapp: "https://movie-recommendation-app-jet.vercel.app/",
+    }
+  ];  
