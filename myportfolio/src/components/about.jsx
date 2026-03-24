d0575ffb40dbcacd0c53bff4caf7152bdc8f9cc9
Devesh-star <kanjarrbacche@gmail.com>
2026-03-25 02:06:31 +0530

    download cv

--- myportfolio/src/components/about.jsx
+++ myportfolio/src/components/about.jsx
@@ -45,7 +45,7 @@ const About = () => {
             efficient solutions.
           </p>
           <a
-            href="../assets/resume.pdf"
+            href="https://drive.google.com/file/d/11F-WfeFfsCuZnj_rzU5_f31HZCbSYrA7/view?usp=sharing"
             target="_blank"
             rel="noopener noreferrer"
             className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
