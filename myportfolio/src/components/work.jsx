2082f3f7dce5e84c8227880b176738293a231693
Devesh-star <kanjarrbacche@gmail.com>
2026-03-29 04:13:12 +0530

    projects fix

--- myportfolio/src/components/work.jsx
+++ myportfolio/src/components/work.jsx
@@ -1,67 +1,71 @@
 import React, { useState } from "react";
 import { projects } from "../constants";
 
-const Work = () => {
+const Projects = () => {
   const [selectedProject, setSelectedProject] = useState(null);
 
-  const handleOpenModal = (project) => {
+  const handelOpenModal = (project) => {
     setSelectedProject(project);
   };
 
-  const handleCloseModal = () => {
+  const handelCloseModal = () => {
     setSelectedProject(null);
   };
 
   return (
     <section
       id="work"
-      className="py-24 pb-24 px-[12vw] md:px-[7vw] lg:px-[20vw] font-sans relative"
+      className="py-24 pb-24 px-[12vw] md:px-[7vw] lg:px-[16vw] font-sans relative"
     >
-      {/* Section Title */}
+      {/* Section title */}
       <div className="text-center mb-16">
-        <h2 className="text-4xl font-bold text-white">PROJECTS</h2>
-        <div className="w-32 h-1 bg-purple-500 mx-auto mt-4"></div>
-        <p className="text-gray-400 mt-4 text-lg font-semibold">
+        <h2 className="text-4xl sm:text-4xl text-white font-bold transition-transform duration-200 hover:scale-110">PROJECTS</h2>
+        <div className="w-32 h-1 bg-[#8245ec] mx-auto mt-2 mb-2"></div>
+        <p className="text-gray-300 text-lg font-semibold">
           A showcase of the projects I have worked on, highlighting my skills
           and experience in various technologies
         </p>
       </div>
 
-      {/* Projects Grid */}
-      <div className="grid gap-12 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
-        {projects.map((project) => (
-          <div
-            key={project.id}
-            onClick={() => handleOpenModal(project)}
-            className="border border-white bg-gray-900 backdrop-blur-md rounded-2xl shadow-2xl overflow-hidden cursor-pointer hover:shadow-purple-500/50 hover:-translate-y-2 transition-transform duration-300"
-          >
-            <div className="p-4">
-              <img
-                src={project.image}
-                alt={project.title}
-                className="w-full h-48 object-cover rounded-xl"
-              />
-            </div>
-            <div className="p-6">
-              <h3 className="text-2xl font-bold text-white mb-2">
-                {project.title}
-              </h3>
-              <p className="text-gray-500 mb-4 pt-4 line-clamp-3">
-                {project.description}
-              </p>
-              <div className="mb-4">
-                {project.tags.map((tag, index) => (
-                  <span
-                    key={index}
-                    className="inline-block bg-[#251f38] text-xs font-semibold text-purple-500 rounded-full px-2 py-1 mr-2 mb-2"
-                  >
-                    {tag}
-                  </span>
-                ))}
+      {/* Project Grid */}
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-12">
+        {projects.map((project) => {
+          return (
+            <div
+              key={project.id}
+              onClick={() => handelOpenModal(project)}
+              className="border border-white bg-gray-900 backdrop-blur-md rounded-2xl overflow-hidden cursor-pointer shadow-2xl hover:shadow-purple-500/50 hover:scale-105 transition-transform duration-300"
+            >
+              <div className="p-4">
+                <img
+                  src={project.image}
+                  alt={project.title}
+                  className="w-full object-cover h-48 rounded-xl"
+                />
+              </div>
+              <div className="p-6">
+                <h3 className="font-bold text-white mb-2 text-2xl">
+                  {project.title}
+                </h3>
+                <p className="text-gray-300 mb-4 pt-4 line-clamp-3">
+                  {project.description}
+                </p>
+                <div>
+                  {project.tags.map((tag, index) => {
+                    return (
+                      <span
+                        key={index}
+                        className="inline-block bg-[#251f38] text-xs font-semibold text-purple-500 rounded-full px-2 py-1 mr-2 mb-2"
+                      >
+                        {tag}
+                      </span>
+                    );
+                  })}
+                </div>
               </div>
             </div>
-          </div>
-        ))}
+          );
+        })}
       </div>
 
       {/* Modal Container */}
@@ -70,37 +74,38 @@ const Work = () => {
           <div className="bg-gray-900 rounded-xl shadow-2xl lg:w-full w-[90%] max-w-3xl overflow-hidden relative">
             <div className="flex justify-end p-4">
               <button
-                onClick={handleCloseModal}
+                onClick={handelCloseModal}
                 className="text-white text-3xl font-bold hover:text-purple-500"
               >
                 &times;
               </button>
             </div>
-
             <div className="flex flex-col">
               <div className="w-full flex justify-center bg-gray-900 px-4">
                 <img
                   src={selectedProject.image}
                   alt={selectedProject.title}
-                  className="lg:w-full w-[95%] object-contain rounded-xl shadow-2xl"
+                  className="lg:w-full w-[95%] h-48 object-contain rounded-xl shadow-2xl"
                 />
               </div>
-              <div className="lg:p-8 p-6">
+              <div className="p-4">
                 <h3 className="lg:text-3xl font-bold text-white mb-4 text-md">
                   {selectedProject.title}
                 </h3>
                 <p className="text-gray-400 mb-6 lg:text-base text-xs">
                   {selectedProject.description}
                 </p>
-                <div className="flex flex-wrap gap-2 mb-6">
-                  {selectedProject.tags.map((tag, index) => (
-                    <span
-                      key={index}
-                      className="bg-[#251f38] text-xs font-semibold text-purple-500 rounded-full px-2 py-1"
-                    >
-                      {tag}
-                    </span>
-                  ))}
+                <div className="flex flex-wrap gap-2 mb-4">
+                  {selectedProject.tags.map((tag, index) => {
+                    return (
+                      <span
+                        key={index}
+                        className="bg-[#251f38] text-xs font-semibold text-purple-500 rounded-full px-2 py-1"
+                      >
+                        {tag}
+                      </span>
+                    );
+                  })}
                 </div>
                 <div className="flex gap-4">
                   <a
@@ -129,4 +134,4 @@ const Work = () => {
   );
 };
 
-export default Work;
+export default Projects;
